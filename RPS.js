f7dbf3e336baa04dd6a82941d27abdaff95414bc
seanfaubus <seanfaubus@gmail.com>
2022-04-09 14:17:09 -0500

    working on a new function

--- RPS.js
+++ RPS.js
@@ -16,7 +16,7 @@ function computerPlay() {
 }
 
 // computer's choice
-const computerChoice = computerPlay();
+// const computerChoice = computerPlay();
 
 // player's turn to decide!
 function userPlay() {
@@ -28,10 +28,10 @@ function userPlay() {
     }  
 
 
-const playerChoice = userPlay();
+// const playerChoice = userPlay();
 
-console.log(computerChoice);
-console.log(playerChoice);
+/* console.log(computerChoice);
+console.log(playerChoice); */
 
 // function to play a game!
 function playRound(user, computer) {
@@ -55,5 +55,42 @@ function playRound(user, computer) {
         return('You lose!');
     }
 }
+// console.log(playRound(playerChoice, computerChoice));
 
-console.log(playRound(playerChoice, computerChoice));
+// creating a game function that will consist of 5 rounds!
+
+function game() {
+    let rounds = 0;
+    let wins = 0;
+    let losses = 0;
+    let draws = 0;
+    // getting user and computer's play for the round...
+    let computerChoice = computerPlay();
+    let playerChoice = userPlay();
+    // while loop to loop through 5 rounds..
+    while (rounds < 5) {
+        let winner = playRound(playerChoice, computerChoice);
+        if (winner === 'You lose!') {
+            losses += 1;
+        } else if (winner === 'You win!') {
+            wins += 1;
+        } else {
+            draws += 1;
+        }
+        rounds++;
+        computerChoice = computerPlay();
+        playerChoice = userPlay();
+    }
+    
+    /* determining the winner after 5 rounds*/
+        if (wins > losses && wins > draws) {
+            return ('You are the winner!');
+        } else if (losses > wins && losses > draws) {
+            return ('You lost. Try again!');
+        } else if (draws > wins && draws > losses) {
+            return ('Weird. You draw too much');
+        }
+        
+    }
+
+console.log(game())
